f9d641de7af90beefb6faff0bf5d48a30e5a5362
minnczi <kmj1995kr@gmail.com>
2023-06-09 21:07:03 +0900

    init commit

---
+++ src/main/java/com/lgcns/tct_backend/TctBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.tct_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TctBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TctBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/tct_backend/TctBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.tct_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TctBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
