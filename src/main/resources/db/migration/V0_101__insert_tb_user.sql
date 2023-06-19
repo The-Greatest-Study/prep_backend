6bc9d5f0b945709306313afa73b0aaa4ac219a9a
jiin-6v6 <ohjiin97@gmail.com>
2023-06-19 20:52:55 +0900

    ADD - TB_USER insert flyway script 추가

---
+++ src/main/resources/db/migration/V0_101__insert_tb_user.sql
@@ -0,0 +1,16 @@
+INSERT INTO TB_USER
+(USER_ID, USER_NAME, PASSWORD, BIRTH_DATE, GENDER, ACTIVE_YN)
+VALUES
+('mino', '김민호', 'mino0517!', '19920829', 'M', 'Y'),
+('hagendaz', '유상욱', 'hagendaz0517!', '19931010', 'M', 'Y'),
+('jimmy', '한진영', 'jimmy0517!', '19930328', 'M', 'Y'),
+('han', '이한별', 'han0517!', '19940217', 'M', 'Y'),
+('junsu', '이준수', 'junsu0517!', '19940801', 'M', 'Y'),
+('minnczi', '김민지', 'minnczi0517!', '19950420', 'F', 'Y'),
+('rex', '박성국', 'rex0517!', '19950717', 'M', 'Y'),
+('j-soo', '장수현', 'jsoo0517!', '19960512', 'F', 'Y'),
+('charlie', '안찬영', 'charlie0517!', '19970217', 'M', 'Y'),
+('coolcool', '오지인', 'coolcool0517!', '19970218', 'F', 'Y'),
+('cathy', '하민주', 'cathy0517!', '19970923', 'F', 'Y'),
+('teri', '김지홍', 'teri0517!', '19970228', 'F', 'Y')
+;
