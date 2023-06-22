547c991114e3248299457d7a9f41e69da3e6331d
jiin-6v6 <ohjiin97@gmail.com>
2023-06-22 20:58:51 +0900

    ADD - tb_restaurant, tb_mz_list_restaurant_rel flyway insert script 추가

---
+++ src/main/resources/db/migration/V0_103__insert_tb_mz_list.sql
@@ -0,0 +1,19 @@
+INSERT INTO TB_MZ_LIST
+(LIST_ID, USER_ID, LIST_NAME, CREATED_DATE)
+VALUES
+('MZ00000001', 'mino', 'mino맛집리스트', '20220530'),
+('MZ00000002', 'mino', 'mino맛집리스트2nd', '20220530'),
+('MZ00000003', 'hagendaz', 'hagendaz맛집리스트', '20210130'),
+('MZ00000004', 'jimmy', 'jimmy맛집리스트', '20230115'),
+('MZ00000005', 'han', 'han맛집리스트', '20210312'),
+('MZ00000006', 'junsu', 'junsu맛집리스트', '20220731'),
+('MZ00000007', 'minnczi', 'minnczi맛집리스트', '20221212'),
+('MZ00000008', 'minnczi', 'minnczi맛집리스트2nd', '20221212'),
+('MZ00000009', 'rex', 'rex맛집리스트', '20230227'),
+('MZ00000010', 'j-soo', 'j-soo맛집리스트', '20230227'),
+('MZ00000011', 'charlie', 'charlie맛집리스트', '20230601'),
+('MZ00000012', 'coolcool', 'coolcool맛집리스트', '20211108'),
+('MZ00000013', 'coolcool', 'coolcool맛집리스트2nd', '20211108'),
+('MZ00000014', 'cathy', 'cathy맛집리스트', '20220508'),
+('MZ00000015', 'teri', 'teri맛집리스트', '20220615')
+;
