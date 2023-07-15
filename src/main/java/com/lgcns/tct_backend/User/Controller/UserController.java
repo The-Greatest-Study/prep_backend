9109ad809dac6d39d9ffea3f3446a6f4f07d469e
minho65 <skt_ks@naver.com>
2023-07-15 15:25:18 +0900

    FIX - fixed url mapping by @RequestMapping

--- src/main/java/com/lgcns/tct_backend/User/Controller/UserController.java
+++ src/main/java/com/lgcns/tct_backend/User/Controller/UserController.java
@@ -1,8 +1,10 @@
 package com.lgcns.tct_backend.User.Controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lgcns.tct_backend.User.Model.User;
@@ -10,14 +12,17 @@ import com.lgcns.tct_backend.User.Service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequiredArgsConstructor 
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController 
 public class UserController {
 
     private final UserService service;
     
-    @GetMapping("/user/{userId}")
+    @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable(name = "userId") String userId){
+        // verification 개선필요
+        if(StringUtils.isBlank(userId)) new IllegalArgumentException("wrong userId");
         return ResponseEntity.ok(service.getUser(userId));
     }
 }
